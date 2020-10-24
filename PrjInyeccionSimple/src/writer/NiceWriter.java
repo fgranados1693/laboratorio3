4aa08c8d3419bd793af2f36a4a8bddd0926ebc28
fgranados1693 <52337176+fgranados1693@users.noreply.github.com>
2020-10-23 18:49:48 -0600

    commit inicio
    Commit con todo el laboratorio 3, curso componentes

---
+++ PrjInyeccionSimple/src/writer/NiceWriter.java
@@ -0,0 +1,9 @@
+package writer;
+import org.springframework.stereotype.Service;
+@Service
+public class NiceWriter implements IWriter {
+@Override
+public void writer(String s) {
+System.out.println("The String is " + s);
+}
+}
---
+++ PrjInyeccionSimple/src/writer/Writer.java
@@ -0,0 +1,7 @@
+package writer;
+public class Writer implements IWriter {
+@Override
+public void writer(String s) {
+System.out.println("+++++-->" + s);
+}
+}
